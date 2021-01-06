9b60571bff5c2b3ee1845d1d3d8a31d2457e5388
Daniel Lutz <daniel.lutz@citavi.com>
2021-01-06 10:10:08 +0100

    init

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # RemoveLocalProjectAddon
+
+# RemoveLocalProjectAddon
+
+This add-on adds a <kbd>Remove project..</kbd> command to the **...** dialog of each **local** project entry on the start form. The command delete the **Attachements folder** and the **parent folder** of the `*.ctv` file.
+
+## Releases
+
+The compiled library can be found under [releases](./../../releases) as an archive.
+
+## Disclaimer
+
+>There are no support claims by the company **Swiss Academic Software GmbH**, the provider of **Citavi** or other liability claims for problems or data loss. Any use is at your own risk. All rights to the name **Citavi** and any logos used are owned by **Swiss Academic Software GmbH**.
+
+## License
+
+This project is licensed under the [MIT](LICENSE) License
---
+++ src/RemoveLocalProjectAddon/Addon.cs
@@ -0,0 +1,102 @@
+﻿using RemoveLocalProject.Properties;
+using SwissAcademic.Citavi;
+using SwissAcademic.Citavi.Settings;
+using SwissAcademic.Citavi.Shell;
+using SwissAcademic.Citavi.Shell.Controls;
+using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace RemoveLocalProject
+{
+    public class Addon : CitaviAddOnEx<ProjectDetailsControl>
+    {
+        public override void OnHostingFormLoaded(ProjectDetailsControl projectDetailsControl)
+        {
+            var WorkingKnownProject = projectDetailsControl
+                                        .GetType()
+                                        .GetProperty("WorkingKnownProject", BindingFlags.Instance | BindingFlags.NonPublic)?
+                                        .GetValue(projectDetailsControl) as KnownProject;
+
+            if (WorkingKnownProject == null || WorkingKnownProject.ProjectType != ProjectType.DesktopSQLite) return;
+
+            var deleteProjectLabel = projectDetailsControl
+                                        .Controls[0]
+                                        .Controls.OfType<SwissAcademic.Controls.Label>()
+                                        .FirstOrDefault(label => label.Name.Equals("deleteProjectLabel", StringComparison.OrdinalIgnoreCase));
+
+            if (deleteProjectLabel == null) return;
+
+            var deleteProjectLabelForDesktopSQLite = new SwissAcademic.Controls.Label
+            {
+                Name = "deleteProjectLabelForDesktopSQLite",
+                Location = deleteProjectLabel.Location,
+                Text = deleteProjectLabel.Text,
+                Style = SwissAcademic.Controls.LabelStyle.StandardLink,
+                TabIndex = 2,
+                TabStop = true
+            };
+
+            deleteProjectLabelForDesktopSQLite.LinkClicked += DeleteProjectLabel_LinkClicked;
+            projectDetailsControl.Controls[0].Controls.Add(deleteProjectLabelForDesktopSQLite);
+        }
+
+        private void DeleteProjectLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            if (sender is SwissAcademic.Controls.Label label && label.Parent?.Parent is ProjectDetailsControl projectDetailsControl)
+            {
+                var WorkingKnownProject = projectDetailsControl
+                                      .GetType()
+                                      .GetProperty("WorkingKnownProject", BindingFlags.Instance | BindingFlags.NonPublic)?
+                                      .GetValue(projectDetailsControl) as KnownProject;
+
+                using (var input = new InputBox(projectDetailsControl))
+                {
+                    input.Title = Resources.DeleteProject_Title;
+                    input.Description = string.Format(Resources.DeleteProject, WorkingKnownProject.Name);
+                    input.InputLabel = string.Empty;
+                    var hnd = new InputBoxValidateEventHandler((s) =>
+                    {
+                        return s == WorkingKnownProject.Name;
+                    });
+                    if (input.ShowDialog(projectDetailsControl, null, hnd) != DialogResult.OK)
+                    {
+                        return;
+                    }
+                }
+
+                try
+                {
+                    if (Directory.Exists(WorkingKnownProject.AttachmentsPath))
+                    {
+                        Directory.Delete(WorkingKnownProject.AttachmentsPath, true);
+                    }
+
+                    if (File.Exists(WorkingKnownProject.ConnectionString))
+                    {
+                        var directory = Path.GetDirectoryName(WorkingKnownProject.ConnectionString);
+
+                        if (Directory.Exists(directory))
+                        {
+                            Directory.Delete(directory, true);
+                        }
+                    }
+                }
+                catch (Exception exp)
+                {
+                    MessageBox.Show(exp.ToString());
+                }
+
+                if (projectDetailsControl.Owner is StartForm startForm)
+                {
+                    Program.Engine.Settings.General.KnownProjects.Validate();
+                    startForm.GetType().GetMethod("RefreshKnownProjectsPanel", BindingFlags.NonPublic | BindingFlags.Instance)?.Invoke(startForm, new object[] { });
+                }
+
+                projectDetailsControl.Close();
+            }
+        }
+    }
+}
